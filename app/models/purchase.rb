a7d4760c00c6659fc807137aaf5b9831940c8d37
github username <github email address>
2021-02-07 13:58:02 -0500

    building models and controller action

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,4 @@
 class Purchase < ApplicationRecord
+    belongs_to :comics
+    belongs_to :users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get '/comics' to: 'comics#index'
+# post '/comics' to: 'comics#create'
+# get '/comics/new' to: 'comics#new', as: 'new_comic'
+
 end
